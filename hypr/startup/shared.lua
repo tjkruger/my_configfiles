f6f2e7482afc2b3eebac6a8039c8c469e591cefd
Tjkruger <krugertjark@gmail.com>
2026-09-25 20:29:57 +0200

    soooo startup not starting ...

--- hypr/startup/shared.lua
+++ hypr/startup/shared.lua
@@ -1,5 +1,4 @@
 -- Shared startup
 hl.on("hyprland.start", function()
-    hl.exec_cmd("awww-daemon")
     hl.exec_cmd("waybar -c ~/.config/hypr/services/shared/waybar/config.jsonc -s ~/.config/hypr/services/shared/waybar/style.css")
 end)
